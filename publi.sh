7d67d80b7b835f5f9b876e05facff39f16bc619c
Jozef Reisinger <jozef.reisinger@telekom.com>
2019-01-23 10:35:56 +0100

    rebuilding

--- publi.sh
+++ publi.sh
@@ -7,17 +7,20 @@
 PUBLIC="$HOME/git/hub/jreisinger.github.io"
 GITMSG="rebuilding public site `date`"
 
+function add_to_git() {
+    local msg="$1"
+    git add .
+    git commit -m "$msg"
+    git push origin master
+}
+
 echo -e "\033[0;32mBulding web pages ...\033[0m"
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 echo -e "\033[0;32mAdding stuff to repo jreisinger.github.io ...\033[0m"
 (cd $PUBLIC && git pull)
 rsync public/* $PUBLIC/
-(cd $PUBLIC && git add .)
-git commit -m "$GITMSG"
-git push origin master
+(cd $PUBLIC && add_to_git $GITMSG)
 
 echo -e "\033[0;32mAdding stuff to repo hugo (CWD) ...\033[0m"
-git add .
-git commit -m "$GITMSG"
-git push
+add_to_git $GITMSG
