3efc6c980c05ec764fc9fc3da449cb7497a08872
Jozef Reisinger <jozef.reisinger@telekom.com>
2019-01-23 10:08:20 +0100

    deploy new stuff

--- publi.sh
+++ publi.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+#
+# Based on https://gohugo.io/hosting-and-deployment/hosting-on-github/ but
+# modified not to use git submodule :-)
+#
+
+PUBLIC="$HOME/git/hub/jreisinger.github.io"
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+mv hugo/* $PUBLIC
 
 # Go To Public folder
-cd public
+cd $PUBLIC
 # Add changes to git.
 git add .
 
@@ -20,8 +27,8 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
-# Come Back up to the Project Root
-cd ..
+# Come back to the source
+cd -
 
 git add .
 git commit -m "deploy new stuff"
